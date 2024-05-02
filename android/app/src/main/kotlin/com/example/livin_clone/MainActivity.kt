535aa6e1d2eb9e74bd94e0154beccb92b5afa365
nasikhunamin <mohamad.amin@phincon.com>
2024-05-02 09:42:19 +0700

    add flag secure:android

--- android/app/src/main/kotlin/com/example/livin_clone/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/livin_clone/MainActivity.kt
@@ -1,6 +1,21 @@
 package com.example.livin_clone
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import android.view.WindowManager.LayoutParams;
 
 class MainActivity: FlutterActivity() {
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine ) {
+        // Add this line to register your plugins
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        
+        // Add FLAG_SECURE to prevent screenshots and screen recordings
+        getWindow().setFlags(LayoutParams.FLAG_SECURE, LayoutParams.FLAG_SECURE)
+    }
+
+    override onPause(){
+        
+    }
 }
