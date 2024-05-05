42aa6d29a519f3c910a8e3f60074221cc988b449
nasikhunamin <mohamad.amin@phincon.com>
2024-05-05 07:44:00 +0700

    add melos localizations

---
+++ shared/localizations/lib/l10n/livin_localizations_en.dart
@@ -0,0 +1,33 @@
+import 'livin_localizations.dart';
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get onboarding1 => 'Welcome to the new Livin';
+
+  @override
+  String get onboarding3 => 'All e-wallet in Livin';
+
+  @override
+  String get onboarding2 => 'No debit card, no worries';
+
+  @override
+  String get descOnboarding1 => 'One stop application for all off your lifestyle and daily needs.';
+
+  @override
+  String get descOnboarding2 => 'More than top-up, check all your e-wallets in one app. Hassle-free guaranted.';
+
+  @override
+  String get descOnboarding3 => 'Left your card at home? Easy, cash out in ATM with Livin.';
+
+  @override
+  String get registerAccount => 'Open My First Account';
+
+  @override
+  String get loginAccount => 'I Have Mandiri Debit/Credit Card';
+
+  @override
+  String get registerProduct => 'Product';
+}
---
+++ shared/localizations/lib/l10n/livin_localizations_id.dart
@@ -0,0 +1,33 @@
+import 'livin_localizations.dart';
+
+/// The translations for Indonesian (`id`).
+class AppLocalizationsId extends AppLocalizations {
+  AppLocalizationsId([String locale = 'id']) : super(locale);
+
+  @override
+  String get onboarding1 => 'Selamat datang di aplikasi Livin by Mandiri Terbaru';
+
+  @override
+  String get onboarding3 => 'Tetap tenang tanpa kartu';
+
+  @override
+  String get onboarding2 => 'Cek e-wallet tanpa ribet';
+
+  @override
+  String get descOnboarding1 => 'Satu aplikasi perbankan untuk gaya hidup dan segala kebutuhan Anda sehari-hari.';
+
+  @override
+  String get descOnboarding2 => 'Lebih dari sekedar top-up. Pantau semua saldo e-wallet kamu dalam satu aplikasi.';
+
+  @override
+  String get descOnboarding3 => 'Lupa bawa kartu, tapi butuh cash? Bisa! \nBagi-bagi cash tanpa ketemu? Bisa!';
+
+  @override
+  String get registerAccount => 'Belum Punya Rekening Mandiri';
+
+  @override
+  String get loginAccount => 'Punya Kartu Debit/Kredit Mandiri';
+
+  @override
+  String get registerProduct => 'Produk';
+}
