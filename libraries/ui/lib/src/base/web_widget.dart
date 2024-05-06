d3b99c9c00ed3105c54637016fbb089befe826f5
nasikhunamin <mohamad.amin@phincon.com>
2024-05-06 07:17:20 +0700

    add flutter_inappwebview

--- libraries/ui/lib/src/base/web_widget.dart
+++ libraries/ui/lib/src/base/web_widget.dart
@@ -1,5 +1,7 @@
+//import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+// import 'package:webview_flutter/webview_flutter.dart';
 
 class WebWidget extends StatefulWidget {
   final Function()? onLoading;
@@ -19,39 +21,90 @@ class WebWidget extends StatefulWidget {
 }
 
 class _WebWidgetState extends State<WebWidget> {
-  late WebViewController controller;
+  //late WebViewController controller;
 
-  bool _isLoading = true;
+  InAppWebViewController? controller;
+
+  // InAppWebViewSettings settings = InAppWebViewSettings(
+  //     isInspectable: kDebugMode,
+  //     mediaPlaybackRequiresUserGesture: false,
+  //     allowsInlineMediaPlayback: true,
+  //     iframeAllow: "camera; microphone",
+  //     iframeAllowFullscreen: true);
 
   @override
   void initState() {
     super.initState();
-    controller = WebViewController()
-      ..loadRequest(Uri.parse(widget.url))
-      ..setNavigationDelegate(NavigationDelegate(onPageStarted: (url) {
-        if (widget.onLoading != null) widget.onLoading!();
-      }, onPageFinished: (url) {
-        setState(() {
-          _isLoading = false;
-        });
-        if (widget.onPageFinished != null) widget.onPageFinished!();
-      }, onNavigationRequest: (navigation) {
-        if (widget.onLoadUrl != null) {
-          widget.onLoadUrl!(navigation.url);
-          return NavigationDecision.prevent;
-        }
-        return NavigationDecision.navigate;
-      }))
-      ..setBackgroundColor(Colors.white)
-      ..setJavaScriptMode(JavaScriptMode.unrestricted);
+    // controller = WebViewController()
+    //   ..loadRequest(Uri.parse(widget.url))
+    //   ..setNavigationDelegate(NavigationDelegate(onPageStarted: (url) {
+    //     if (widget.onLoading != null) widget.onLoading!();
+    //   }, onPageFinished: (url) {
+    //     setState(() {
+    //       _isLoading = false;
+    //     });
+    //     if (widget.onPageFinished != null) widget.onPageFinished!();
+    //   }, onNavigationRequest: (navigation) {
+    //     if (widget.onLoadUrl != null) {
+    //       widget.onLoadUrl!(navigation.url);
+    //       return NavigationDecision.prevent;
+    //     }
+    //     return NavigationDecision.navigate;
+    //   }))
+    //   ..setBackgroundColor(Colors.white)
+    //   ..setJavaScriptMode(JavaScriptMode.unrestricted);
+
+    controller?.reload();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_isLoading && widget.onLoading == null) {
-      return const Center(child: CircularProgressIndicator());
-    }
+    //return WebViewWidget(controller: controller);
 
-    return WebViewWidget(controller: controller);
+    return InAppWebView(
+      key: const ValueKey("web_widget"),
+      initialUrlRequest: URLRequest(url: Uri.parse(widget.url)),
+      //initialSettings: settings,
+      initialOptions: InAppWebViewGroupOptions(
+          crossPlatform: InAppWebViewOptions(
+            useShouldOverrideUrlLoading: true,
+            mediaPlaybackRequiresUserGesture: false,
+          ),
+          android: AndroidInAppWebViewOptions(
+            useHybridComposition: false,
+          ),
+          ios: IOSInAppWebViewOptions(
+            allowsInlineMediaPlayback: true,
+          )),
+      onLoadStart: (controller, url) {
+        if (widget.onLoading != null) widget.onLoading!();
+      },
+      onLoadStop: (controller, url) {
+        if (widget.onPageFinished != null) widget.onPageFinished!();
+      },
+      shouldOverrideUrlLoading: (controller, navigationAction) async {
+        if (widget.onLoadUrl != null) {
+          widget.onLoadUrl!(navigationAction.request.url.toString());
+
+          return NavigationActionPolicy.CANCEL;
+        }
+
+        return NavigationActionPolicy.ALLOW;
+      },
+      onProgressChanged: (controller, progress) {
+        if (widget.onLoading != null && progress < 100) widget.onLoading!();
+        if (widget.onPageFinished != null && progress >= 100) {
+          widget.onPageFinished!();
+        }
+      },
+      onWebViewCreated: (controller) {
+        this.controller = controller;
+      },
+    );
   }
 }
